c97995515ef2e2f2dba99d32445703dd80abda36
emovies <emovies@DESKTOP-DD3G9AG>
2024-03-04 05:34:17 +0300

    modified:   components/footer.php

--- components/footer.php
+++ components/footer.php
@@ -23,7 +23,7 @@
     <script src="assets/js/jquery-1.11.0.min.js"></script>
     <script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/captures.js"></script>
+    <script src="assets/js/wag.js"></script>
     <script src="assets/js/custom.js"></script>
     <!-- End Script -->
 </body>
